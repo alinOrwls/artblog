71dbfc83acb63127ba38d87f2d9d383828cca9fd
alinOrwls <71549673+alinOrwls@users.noreply.github.com>
2024-02-05 19:52:53 +0000

    new added code

--- Javascript_assignment/script.js
+++ Javascript_assignment/script.js
@@ -1,20 +1,78 @@
-const buttons = document.querySelectorAll("[data-carousel-button]")
 
-buttons. forEach(button => { 
-    button.addEventListener('click', () => {
-    const offset =  button.dataset.carouselButton === "next"  ?  1  :  -1
-    const slides  = button
-        .closest("[data.carousel]")
-        .querySelector("[data-slides")
 
-        const activeSlide = slides.querySelector("[data-active]")
-        let newIndex = [...slides.children].indexOf(activeSlide) + offset
-        if (newIndex <  0) newIndex  = slides.children.length -1
-        if(newIndex >= slides.children.length) newIndex = 0 
 
-        slides.children[ newIndex].dataset.active = true
-        delete activeSlide.dataset.active
-     })
-})
-    
+
+//form
+
+const data = {
+    name: '  ',
+    email: '  ',
+    message: '  '
+}
+
+const name= document.querySelector('#name');
+const email= document.querySelector('#email');
+const message= document.querySelector('#message');
+const form= document.querySelector('.formulario')
+
+name.addEventListener('input',  reedtext);
+email.addEventListener('input',  reedtext);
+message.addEventListener('input',  reedtext);
+
+//submit event
+form.addEventListener('submit' , function( event){
+     event.preventDefault();
+
+     //validation
+
+     const  { name, email, message } = data;
+     
+        if (name === '  ' || email === ' ' || message === '  ')  {
+            alert (' Please fill the form')
+
+               return;
+        }
+
+// alert send
+        sending(' All done!! we will  be in contact with you')
+
+});
+
+function reedtext(e) {
+        // console.log(e.target.value)
+
+        data[e.target.id] = e.target.value;
+
+        //console.log(data)
+}
+
+//showError
+ function alert (mensaje) {
+    const error = document.createElement('P');
+    error.textContent = mensaje;
+    error.classList.add('error');
+
+   form.appendChild (error);
+
+   setTimeout (() => {
+    error.remove();
+
+   }, 5000);
+ }
+
+ function sending(mensaje) {
+    const send = document.createElement('P');
+    send.textContent= mensaje;
+    send.classList.add('send');
+
+    form.appendChild(send)
+
+    setTimeout (() =>{
+        send.remove();
+
+
+    }, 5000);
+
+ }
+
 
